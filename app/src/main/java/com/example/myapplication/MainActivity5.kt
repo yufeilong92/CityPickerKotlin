fc1a3f943537333b81933c25d42843ddbfebf4c3
yufeilong92 <931697478@qq.com>
2020-09-10 11:21:31 +0800

    修改数据

--- app/src/main/java/com/example/myapplication/MainActivity5.kt
+++ app/src/main/java/com/example/myapplication/MainActivity5.kt
@@ -1,11 +1,9 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main9.*
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class MainActivity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +22,7 @@ class MainActivity5 : AppCompatActivity() {
         rlv_main6_content.layoutManager=mll
         rlv_main6_content.adapter=adaapter
 
+        val hasp=HashMap<Any,Any?>()
 
     }
 
--- app/src/main/java/com/example/myapplication/MainActivity6.kt
+++ app/src/main/java/com/example/myapplication/MainActivity6.kt
@@ -1,9 +1,10 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
-import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main9.*
 
 class MainActivity6 : AppCompatActivity() {
@@ -13,7 +14,7 @@ class MainActivity6 : AppCompatActivity() {
         bindViewData()
     }
     private fun bindViewData() {
-        val list = mutableListOf<String>()
+  /*      val list = mutableListOf<String>()
         for (item in 0..200) {
             list.add("$item")
         }
@@ -28,6 +29,7 @@ class MainActivity6 : AppCompatActivity() {
                     Toast.makeText(this, "$position et_text_content", Toast.LENGTH_SHORT).show();
                 }
                 R.id.tv_text_content->{
+
                     Toast.makeText(this, "$position tv_text_content", Toast.LENGTH_SHORT).show();
                 }
                 R.id.tv_text_contentone->{
@@ -37,7 +39,14 @@ class MainActivity6 : AppCompatActivity() {
                     Toast.makeText(this, "$position tv_text_contenttwo", Toast.LENGTH_SHORT).show();
                 }
             }
-        }
+        }*/
 
+        val end = tv_main_6.paint.textSize*tv_main_6.text.length
+        val colors= intArrayOf(Color.RED,Color.GREEN,Color.BLUE)
+        val postion= floatArrayOf(0f,0.5f,1.0f)
+
+        val linear=LinearGradient(0f,0f,end,0f,colors,postion,Shader.TileMode.CLAMP)
+        tv_main_6.paint.shader=linear
+        tv_main_6.invalidate()
     }
 }
